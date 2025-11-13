283e3cb6e04975831d846af6de400a37d1a64bc0
Pedro <Pedro@mail.com>
2025-11-13 11:53:54 +0100

    noe

--- connect/includes/compte.inc.php
+++ connect/includes/compte.inc.php
@@ -1,16 +1,11 @@
 <?php
 
-require('../../includes/mariadb.php');
-
-function verif_connexion($email, $mdp){
-        $stmt = $connexion->prepare('SELECT Email, MotPass FROM admin WHERE Email = :email');
-        $stmt->execute(['email' => $email]);
-        $trouver = $stmt->fetch(PDO::FETCH_ASSOC); 
-
-        if($trouver && password_verify($mdp, $trouver['MotPass'])){
-            return true;
-        }else{
-            return false;
-        }
-    }
+require('includes/connect.php');
+        $sql = "select * from acab_t_user where SID = :id_scan"; //préparation de la requête
+        //debug_show($sql);
+        $requete = $connection->prepare($sql);
+        $requete->bindValue(':id_scan', $sid);
+        $requete->execute();
+        $result = $requete->fetch(PDO::FETCH_ASSOC);
+        
 ?>
