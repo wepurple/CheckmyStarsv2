ac034cfd16c8bffef2cfe10bfd72aa14347c2d6e
Pedro <pedro@mail.com>
2025-11-17 18:36:56 +0100

    bdd

--- connect/includes/compte.inc.php
+++ connect/includes/compte.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 require('includes/connect.php');
-        $sql = "select * from acab_t_user where SID = :id_scan"; //préparation de la requête
+        $sql = "select * from checkmystars where SID = :id_scan"; //préparation de la requête
         //debug_show($sql);
         $requete = $connection->prepare($sql);
         $requete->bindValue(':id_scan', $sid);
