ab0794a430c0a1803842621ac60e59e839f5b391
Pedro <pedro@mail.com>
2025-11-17 18:43:16 +0100

    tab

--- includes/mariadb.php
+++ includes/mariadb.php
@@ -1,18 +1,18 @@
 <?php
-// Connection au serveur
-try { //try
+    // Connection au serveur
+    try { //try
 
-    $dns = 'mysql:host=localhost;port=3307;dbname=checkmystars'; //indication sur le SGBDR utilisé et le nom de la base de données qu'on souhaite utilisé
-    $utilisateur = 'root'; //identifiant
-    $motDePasse = 'password'; //mot de passe
+        $dns = 'mysql:host=localhost;port=3307;dbname=checkmystars'; //indication sur le SGBDR utilisé et le nom de la base de données qu'on souhaite utilisé
+        $utilisateur = 'root'; //identifiant
+        $motDePasse = 'password'; //mot de passe
 
-    $connexion = new PDO( $dns, $utilisateur, $motDePasse ); // connnexion à la base de données
-    $connexion->exec("USE checkmystars");
-    $connexion->query("SET NAMES utf8"); // utilisation de l'encodage utf8 pour les accents et autres
-    
-} catch ( Exception $e ) { // capture de l'erreur si il y en a une
-    echo "Connexion à MariaDB impossible : ", $e->getMessage(); // affichage de l'erreur survenue lors de l'échec à la connexion
-die(); // arrêt du code
+        $connexion = new PDO( $dns, $utilisateur, $motDePasse ); // connnexion à la base de données
+        $connexion->exec("USE checkmystars");
+        $connexion->query("SET NAMES utf8"); // utilisation de l'encodage utf8 pour les accents et autres
+        
+    } catch ( Exception $e ) { // capture de l'erreur si il y en a une
+        echo "Connexion à MariaDB impossible : ", $e->getMessage(); // affichage de l'erreur survenue lors de l'échec à la connexion
+    die(); // arrêt du code
 
-}
+    }
 ?> 
