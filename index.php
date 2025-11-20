a42174f96b0d22e4025cec1d9aa78fbccedbdda3
Pedro <Pedro@mail.com>
2025-11-20 08:35:05 +0100

    logo

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel="stylesheet" href="bootstrap 5.3/css/style.css">
   <script src="includes/index.js"></script>
+  <link rel="icon" type="image/x-icon" href="pictures/logo.png">
 </head>
 
 <body>
