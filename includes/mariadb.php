edb4cdb8952bd33876c2a622997d93a5fd84e019
terence-git <temartinant@stpbb.org>
2025-11-20 09:50:38 +0100

    changement connexion bdd

--- includes/mariadb.php
+++ includes/mariadb.php
@@ -1,18 +1,24 @@
 <?php
     // Connection au serveur
-    try { //try
+class Database {
+    private $host = "localhost";
 
-        $dns = 'mysql:host=localhost;port=3307;dbname=checkmystars'; //indication sur le SGBDR utilisé et le nom de la base de données qu'on souhaite utilisé
-        $utilisateur = 'root'; //identifiant
-        $motDePasse = 'password'; //mot de passe
+    private $dns = 'mysql:host=localhost;port=3307;dbname=checkmystars'; //indication sur le SGBDR utilisé et le nom de la base de données qu'on souhaite utilisé
+    private $utilisateur = 'root'; //identifiant
+    private $motDePasse = 'password'; //mot de passe
 
-        $connexion = new PDO( $dns, $utilisateur, $motDePasse ); // connnexion à la base de données
-        $connexion->exec("USE checkmystars");
-        $connexion->query("SET NAMES utf8"); // utilisation de l'encodage utf8 pour les accents et autres
-        
-    } catch ( Exception $e ) { // capture de l'erreur si il y en a une
-        echo "Connexion à MariaDB impossible : ", $e->getMessage(); // affichage de l'erreur survenue lors de l'échec à la connexion
-    die(); // arrêt du code
+    public function getConnection() {
+        $this->connexion = null;
 
+        try {
+            $this->connexion = new PDO( $dns, $utilisateur, $motDePasse ); // connnexion à la base de données
+            $this->connexion->exec("USE checkmystars");
+            $this->connexion->query("SET NAMES utf8");
+        } catch (PDOException $e) {
+            echo "Erreur de connexion : " . $e->getMessage();
+        }
+        return $this->connexion;
     }
+}
+      
 ?> 
