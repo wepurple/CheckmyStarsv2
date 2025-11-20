25bdbde133152d90d11cd8626a9090a3ce4d295c
Pedro <Pedro@mail.com>
2025-11-20 11:18:51 +0100

    not true

--- includes/index.js
+++ includes/index.js
@@ -1,7 +1,6 @@
 function login() {
     //si les champs sont remplis et corrects, pour ne pas envoyer de requetes inutiles au serv
     checkMail = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/.test(document.getElementById("email"))
-    checkMail = true
     if (checkMail && document.getElementById("password")!=""){
         const email = document.getElementById("email").value;
         const password = document.getElementById("password").value;
