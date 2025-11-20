341669ad4f5b9cc2761eb6d34c76240a555f12b3
terence-git <temartinant@stpbb.org>
2025-11-20 09:53:47 +0100

    changement bozo

--- includes/mariadb.php
+++ includes/mariadb.php
@@ -2,16 +2,16 @@
     // Connection au serveur
 class Database {
     private $host = "localhost";
-
-    private $dns = 'mysql:host=localhost;port=3307;dbname=checkmystars'; //indication sur le SGBDR utilisé et le nom de la base de données qu'on souhaite utilisé
-    private $utilisateur = 'root'; //identifiant
-    private $motDePasse = 'password'; //mot de passe
+    private $connexion;
+    private $dns = 'mysql:host=localhost;port=3307;dbname=checkmystars';
+    private $utilisateur = 'root';
+    private $motDePasse = 'password';
 
     public function getConnection() {
         $this->connexion = null;
 
         try {
-            $this->connexion = new PDO( $dns, $utilisateur, $motDePasse ); // connnexion à la base de données
+            $this->connexion = new PDO($this->dns, $this->utilisateur, $this->motDePasse);
             $this->connexion->exec("USE checkmystars");
             $this->connexion->query("SET NAMES utf8");
         } catch (PDOException $e) {
@@ -20,5 +20,4 @@ class Database {
         return $this->connexion;
     }
 }
-      
-?> 
+?>
