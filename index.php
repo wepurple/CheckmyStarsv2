dacd080195683dcbdef59ebdc202ff8e43eb37f1
Pedro <Pedro@mail.com>
2025-11-20 12:00:30 +0100

    toast

--- index.php
+++ index.php
@@ -52,6 +52,16 @@
 
     </div>
 
+    <!-- toast -->
+    <div class="toast align-items-center text-bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="d-flex">
+            <div class="toast-body">
+                Connexion échouée
+            </div>
+            <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+    </div>
+
   </div>
 </body>
 </html>
