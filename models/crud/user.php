c6ec10f6c109b3c8191829467bcf3be7c5e69682
terence-git <temartinant@stpbb.org>
2025-11-20 09:18:17 +0100

    creeation models pour user

---
+++ models/crud/user.php
@@ -0,0 +1,77 @@
+<?php
+class User {
+    private $connexion;
+    private $table = "personnes";
+
+    public $IdPersonne;
+    public $Nom;
+    public $Prenom;
+    public $Civilite;
+    public $Telephone;
+    public $Email;
+    public $Adresse;
+    public $Complement;
+    public $CodePostal;
+    public $Ville;
+    public $Pays;
+    public $DateEnregistrement;
+    public $Societe;
+    public $Role;
+    public $Login;
+    public $MotDePasse;
+
+    /**
+     * Constructeur avec $db pour la connexion à la base de données
+     *
+     * @param $db
+     */
+    public function __construct($db){
+        $this->connexion = $db;
+    }
+    /**
+        * Créer un produit
+         *
+        * @return void
+    */
+    public function creer(){
+        $sql = "INSERT INTO " . $this->table . " SET Nom=:Nom, Prenom=:Prenom, Civilite=:Civilite, Telephone=:Telephone, Email=:Email, Adresse=:Adresse, Complement=:Complement, CodePostal=:CodePostal, Ville=:Ville, Pays=:Pays, DateEnregistrement=:DateEnregistrement, Societe=:Societe, Role=:Role, Login=:Login, MotDePasse=:MotDePasse";
+        $query = $this->connexion->prepare($sql);
+
+        $this->Nom=htmlspecialchars(strip_tags($this->Nom));
+        $this->Prenom=htmlspecialchars(strip_tags($this->Prenom));
+        $this->Civilite=htmlspecialchars(strip_tags($this->Civilite));
+        $this->Telephone=htmlspecialchars(strip_tags($this->Telephone));
+        $this->Email=htmlspecialchars(strip_tags($this->Email));
+        $this->Adresse=htmlspecialchars(strip_tags($this->Adresse));
+        $this->Complement=htmlspecialchars(strip_tags($this->Complement));
+        $this->CodePostal=htmlspecialchars(strip_tags($this->CodePostal));
+        $this->Ville=htmlspecialchars(strip_tags($this->Ville));
+        $this->Pays=htmlspecialchars(strip_tags($this->Pays));
+        $this->DateEnregistrement=htmlspecialchars(strip_tags($this->DateEnregistrement));
+        $this->Societe=htmlspecialchars(strip_tags($this->Societe));
+        $this->Role=htmlspecialchars(strip_tags($this->Role));
+        $this->Login=htmlspecialchars(strip_tags($this->Login));
+        $this->MotDePasse=htmlspecialchars(strip_tags($this->MotDePasse));
+
+        $query->bindParam(":Nom", $this->Nom);
+        $query->bindParam(":Prenom", $this->Prenom);
+        $query->bindParam(":Civilite", $this->Civilite);
+        $query->bindParam(":Telephone", $this->Telephone);
+        $query->bindParam(":Email", $this->Email);
+        $query->bindParam(":Adresse", $this->Adresse);
+        $query->bindParam(":Complement", $this->Complement);
+        $query->bindParam(":CodePostal", $this->CodePostal);
+        $query->bindParam(":Ville", $this->Ville); 
+        $query->bindParam(":Pays", $this->Pays);
+        $query->bindParam(":DateEnregistrement", $this->DateEnregistrement);
+        $query->bindParam(":Societe", $this->Societe);
+        $query->bindParam(":Role", $this->Role);
+        $query->bindParam(":Login", $this->Login);
+        $query->bindParam(":MotDePasse", $this->MotDePasse);
+
+        if($query->execute()){
+            return true;
+        }
+        return false;
+    }
+}
