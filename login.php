e6a68169556c67045aad3771d1e6dc20577081b6
Pedro <Pedro@mail.com>
2025-11-20 09:27:26 +0100

    =

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
     $login = $_REQUEST['email'];
     $password = $_REQUEST['password'];
 
-    $sql("select * from personne where login = :login and MotPasse = :password");
+    $sql=("select * from personne where login = :login and MotPasse = :password");
     $connection->bindParam(':login', $login);
     $connection->bindParam(':password', $password);
     $connection->prepare();
