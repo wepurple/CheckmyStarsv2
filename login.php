456d53e2115607ad627257cbaaa09ee49d6f706f
Pedro <Pedro@mail.com>
2025-11-20 09:57:00 +0100

    test

--- login.php
+++ login.php
@@ -7,11 +7,12 @@
 
     $sql=("select * from personne where login = :login and MotPasse = :password");
     $requete = new Database();
-    
-    $requete->prepare($sql);
-    $requete->bindValue(':login', $login);
-    $requete->bindValue(':password', $password);
-    $requete->execute();
+    $requete.getConnection();
+
+    $requete.getConnection()->prepare($sql);
+    $requete.getConnection()->bindValue(':login', $login);
+    $requete.getConnection()->bindValue(':password', $password);
+    $requete.getConnection()->execute();
     $result = $requete->fetch(PDO::FETCH_ASSOC);
 
     echo(json_encode(array("test"=>"a", "test2"=>"b")));
