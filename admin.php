dc78217cf691abe1df6803ae6bcec4c9d918e77b
Pedro <Pedro@mail.com>
2025-11-27 10:38:49 +0100

    ok

--- admin.php
+++ admin.php
@@ -2,15 +2,13 @@
     session_start();
 
     //Si on tente d'accéder à la page via l'url sans être connecté, on se fait dégager avant de charger la page
-    /*
     if(!isset($_SESSION['Role'])){
         header('Location: deco.php');
         die();
-    } else if($_SESSION['Role']!="Administrateur"){
+    } else if(!$_SESSION['Role']['Administrateur']){
         header('Location: deco.php');
         die();
     }
-        */
 
     require("./includes/navbar.php");
 
--- includes/navbar.php
+++ includes/navbar.php
@@ -34,7 +34,7 @@
                 </ul>
 
                 <div class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <span class="nav-text">Connecté en tant que <?php if(isset($_SESSION['Login'])){echo($_SESSION['Login']);} ?></span>
+                    <span class="nav-text">Connecté en tant que <?php if(isset($_SESSION['Login'])){echo($_SESSION['Prenom']." ").$_SESSION['Nom'];} ?></span>
                 </div>
 
                 <ul class="navbar-nav ms-2 mb-2 mb-lg-0">
