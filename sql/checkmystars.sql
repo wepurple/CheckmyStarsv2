c972a000e3f509c7435eb9577b512dd952d4db73
Pedro <Pedro@mail.com>
2025-11-27 08:21:33 +0100

    sql

--- sql/checkmystars.sql
+++ sql/checkmystars.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3307
--- Généré le : ven. 21 nov. 2025 à 11:56
+-- Généré le : jeu. 27 nov. 2025 à 07:20
 -- Version du serveur : 11.5.2-MariaDB
 -- Version de PHP : 8.3.14
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `checkmystars`
 --
+CREATE DATABASE IF NOT EXISTS `checkmystars` DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci;
+USE `checkmystars`;
 
 -- --------------------------------------------------------
 
