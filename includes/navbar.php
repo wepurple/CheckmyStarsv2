eab88b4f11cb78cd7d02278b054c102fc30d1b95
Pedro <Pedro@mail.com>
2025-11-27 08:13:01 +0100

    nav

--- includes/navbar.php
+++ includes/navbar.php
@@ -37,8 +37,7 @@
                     <span class="nav-text">Connecté en tant que <?php if(isset($_SESSION['Login'])){echo($_SESSION['Login']);} ?></span>
                 </div>
 
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-
+                <ul class="navbar-nav ms-2 mb-2 mb-lg-0">
 
                     <li class="nav-item">
                         <a class="nav-link" href="deco.php">Déconnexion</a>
