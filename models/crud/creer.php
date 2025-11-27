3c9b441dadec4a1c341e19ed1584b6562200dca6
terence-git <temartinant@stpbb.org>
2025-11-27 10:46:30 +0100

    z

--- models/crud/creer.php
+++ models/crud/creer.php
@@ -21,7 +21,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $user->Civilite = $data->Civilite;
         $user->Telephone = $data->Telephone;
         $user->Email = $data->Email;
-        $user->Adresse = $data->Adresse;
+        $user->AdresseNum = $data->AdresseNum;
+        $user->AdresseNom = $data->AdresseNom;
         $user->Complement = $data->Complement;
         $user->CodePostal = $data->CodePostal;
         $user->Ville = $data->Ville;
@@ -31,13 +32,21 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $user->Login = $data->Login;
         $user->MotPasse = $data->MotPasse;
 
-        if($user->creer()){
+        if($user->creerAdresse()){
             http_response_code(201);
-            echo json_encode(array("message" => "Utilisateur créé avec succès."));
+            echo json_encode(array("message" => "Adresse créée avec succès."));
         } else {
             http_response_code(503);
-            echo json_encode(array("message" => "Impossible de créer l'utilisateur."));
+            echo json_encode(array("message" => "Impossible de créer l'adresse."));
         }
+
+        #if($user->creer()){
+            #http_response_code(201);
+            #echo json_encode(array("message" => "Utilisateur créé avec succès."));
+        #} else {
+            #http_response_code(503);
+            #echo json_encode(array("message" => "Impossible de créer l'utilisateur."));
+        #}
     } else {
         http_response_code(400);
         echo json_encode(array("message" => "Erreur, les données entre ne sont pas valide."));
