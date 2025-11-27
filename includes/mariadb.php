52b9c89e5d4715014dc0f3151bdffdfa20248865
Pedro <Pedro@mail.com>
2025-11-27 08:04:04 +0100

    test

--- includes/mariadb.php
+++ includes/mariadb.php
@@ -5,7 +5,7 @@
         private $connexion;
         private $dns = 'mysql:host=localhost;port=3307;dbname=checkmystars';
         private $utilisateur = 'root';
-        private $motDePasse = 'passwod';
+        private $motDePasse = 'password';
 
         public function getConnection() {
             $this->connexion = null;
