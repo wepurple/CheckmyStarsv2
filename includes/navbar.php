de5aba376685dc0dcadaa11dd6cb530fd5cfa59c
Pedro <Pedro@mail.com>
2025-11-27 08:08:18 +0100

    nav

--- includes/navbar.php
+++ includes/navbar.php
@@ -33,7 +33,7 @@
 
                 </ul>
 
-                    <div class="container-fluid">
+                    <div class="navbar-nav container-fluid">
                         <span class="nav-text">Connecté en tant que <?php if(isset($_SESSION['Login'])){echo($_SESSION['Login']);} ?></span>
                     </div>
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
