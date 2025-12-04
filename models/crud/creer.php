978a71897671a7e76957e5c3792d7e8b02a87df2
DVZAngelNCTS <angelnctseuw@gmail.com>
2025-12-04 09:50:32 +0100

    test fix

--- models/crud/creer.php
+++ models/crud/creer.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(0);
+ini_set('display_errors', 0);
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
