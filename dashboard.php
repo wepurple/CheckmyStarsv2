5df722555b0538a9f7e39d44d233152685be1d3f
Moliere <kimoliere@stpbb.org>
2025-12-04 08:55:02 +0100

    dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Gestion inspecteurs - CheckMyStars</title>
+
+        <link rel="stylesheet" href="bootstrap 5.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+        <script src="bootstrap 5.3/js/bootstrap.js"></script>
+        <script src="js/search_inspecteurs.js"></script>
+        <link rel="icon" type="image/x-icon" href="pictures/logosm.png">
+    </head>
+
+    <body class="bg-secondary">
+        <?php        
+            require("./includes/navbar.php");
+        ?>
