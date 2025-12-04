d8ee2daaefe6742843b01330acc20da76939b459
Pedro <Pedro@mail.com>
2025-12-04 12:58:03 +0100

    search

--- js/search_inspecteurs.js
+++ js/search_inspecteurs.js
@@ -7,17 +7,31 @@ function clearTab(){
     }
 }
 
+function updateTab(result, k){
+    clearTab()
+    tab = document.getElementById("table-body")
+
+    for (let i = 0; i < result.length; i++){
+        //console.log(result[i])
+        tab.appendChild(document.createElement("tr"))
+
+        e=tab.lastElementChild
+        e.appendChild(document.createElement('th'))
+        e.lastElementChild.scope = "row"
+        e.lastElementChild.textContent = result[i]["Utilisateur_ID"]
+
+        for(let j=1;j<5;j++){
+            e.appendChild(document.createElement('td'))
+            console.log(result[i][k[j]])
+            e.lastElementChild.textContent = result[i][k[j]]
+        }
+    }    
+}
+
 document.addEventListener("DOMContentLoaded", function() {//quand la page est chargée
 
-    let timer
-    document.getElementById('recherche').addEventListener('input', function(){
-        clearTimeout(timer)
-        console.log("ok")
-        //continuer ici
-    })
-
     const request = new XMLHttpRequest()
-    request.open("GET", `getInspecteurs.php`, true)
+    request.open("POST", `getInspecteurs.php`, true)
     request.send()
     request.onreadystatechange = function(){
         if (request.readyState === 4 && request.status === 200){
@@ -25,26 +39,18 @@ document.addEventListener("DOMContentLoaded", function() {//quand la page est ch
             if(result){
                 k = Object.keys(result[0])
             }
-
-            clearTab()
-            tab = document.getElementById("table-body")
-
-            for (let i = 0; i < result.length; i++){
-                console.log(result[i])
-                tab.appendChild(document.createElement("tr"))
-
-                e=tab.lastElementChild
-                e.appendChild(document.createElement('th'))
-                e.lastElementChild.scope = "row"
-                e.lastElementChild.textContent = result[i]["Utilisateur_ID"]
-
-                for(let j=1;j<5;j++){
-                    e.appendChild(document.createElement('td'))
-                    console.log(result[i][k[j]])
-                    e.lastElementChild.textContent = result[i][k[j]]
-                }
-            }
+            updateTab(result, k)
         }
-    }
+    }   
+
+    let timer
+    document.getElementById('recherche').addEventListener('input', function(){
+        clearTimeout(timer)
+        timer = setTimeout(function(){
+            console.log("Recherche lancée")
+            form = new FormData()
+            form.append(document.getElementById('recherche').name, document.getElementById('recherche').value) /* faut améiliorer ça */
+        }, 300)
+    })
 });
 
