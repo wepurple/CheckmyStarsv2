bca25f1ca013933fbca0358fbe9fdc2998c807e3
terence-git <temartinant@stpbb.org>
2025-12-11 11:29:49 +0100

    test

--- dashboard.php
+++ dashboard.php
@@ -53,6 +53,10 @@
                 </thead>
                 <tbody>
                     <?php
+                    $apiUrl = "models/crud/infoDossier.php";
+                    $response = file_get_contents($apiUrl);
+                    $data = json_decode($response, true);
+                    dump($data);
                     require_once('./includes/mariadb.php');
                     
                     $database = new Database();
