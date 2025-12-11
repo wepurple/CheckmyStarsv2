2e2982ee3a60ca7d05a727227dbb49e94823bb11
LeGoatKylian <kylebigot@stpbb.org>
2025-12-11 11:11:34 +0100

    hf

--- models/crud/afficherUtilisateur.php
+++ models/crud/afficherUtilisateur.php
@@ -22,20 +22,20 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
             extract($row);
  
             $liste = [
-                "Utilisateur_ID" => $IdPersonne,
-                "Utilisateur_Nom" => $Nom,
-                "Utilisateur_Prenom" => $Prenom,
-                "Utilisateur_Civilite" => $Civilite,
-                "Utilisateur_Telephone" => $Telephone,
-                "Utilisateur_Mail" => $Email,
-                "Utilisateur_Societe" => $Societe,
-                "Utilisateur_Signature" => $Signature,
+                "Utilisateur_ID" => $Utilisateur_ID,
+                "Utilisateur_Nom" => $Utilisateur_Nom,
+                "Utilisateur_Prenom" => $Utilisateur_Prenom,
+                "Utilisateur_Civilite" => $Utilisateur_Civilite,
+                "Utilisateur_Telephone" => $Utilisateur_Telephone,
+                "Utilisateur_Mail" => $Utilisateur_Mail,
+                "Utilisateur_Societe" => $Utilisateur_Societe,
+                "Utilisateur_Signature" => $Utilisateur_Signature,
                 "AdressePostale_NumeroRue" => $NumeroRue,
                 "AdressePostale_Complement" => $Complement,
                 "AdressePostale_CodePostal" => $CodePostal,
-                "AdressePostale_NomRue" => $NomRue,
-                "AdressePostale_Ville" => $Ville,
-                "AdressePostale_Pays" => $Pays,
+                "AdressePostale_NomRue" => $AdressePostale_NomRue,
+                "AdressePostale_Ville" => $AdressePostale_Ville,
+                "AdressePostale_Pays" => $AdressePostale_Pays,
             ];
             $tableauClient['utilisateur'][] = $liste;
         }
