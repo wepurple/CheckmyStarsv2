97244db36e4677948d7804c862e41edff86503eb
LeGoatKylian <kylebigot@stpbb.org>
2025-12-11 11:45:48 +0100

    DG4AY5ERT UTJD KGKN

--- models/crud/user.php
+++ models/crud/user.php
@@ -179,6 +179,9 @@ class User {
         $query1 = $this->connexion->prepare($sql1);
         $query1->bindParam(":id", $this->IdPersonne, PDO::PARAM_INT);
         $query1->execute();
-        return $query;
+        return [
+            "query" => $query,
+            "query1" => $query1
+        ];
     }
 }
