002923921e081dac323ba09bbbbceba24dc7b03a
DVZAngelNCTS <angelnctseuw@gmail.com>
2025-12-11 11:29:46 +0100

    fix

--- includes/navbar.php
+++ includes/navbar.php
@@ -29,7 +29,7 @@
                     <a class="nav-link<?php if(strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/gestion_dossiers.php"){echo(' active" aria-current="page');} ?>" href="gestion_dossiers.php">Gestion des dossiers</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link<?php if(strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/critereBack.php"){echo(' active" aria-current="page');} ?>" href="critereBack.php">gestions des critères</a>
+                    <a class="nav-link<?php if(strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/critereBack.php"){echo(' active" aria-current="page');} ?>" href="critereBack.php">Gestions des critères</a>
                 </li>
 
             </ul>
