abb84b79d501a46c37a52882fde8bf01ae25480c
terence-git <temartinant@stpbb.org>
2025-12-18 11:34:44 +0100

    fix

--- dashboard.php
+++ dashboard.php
@@ -89,6 +89,7 @@
                             echo "<td>" . htmlspecialchars($row['Utilisateur_Telephone']) . "</td>";
                             echo "<td>" . htmlspecialchars($row['Utilisateur_Mail']) . "</td>";
                             echo "<td>" . htmlspecialchars($row['Nombre_Dossiers']) . "</td>";
+                            echo "<td>" . htmlspecialchars($row['Status_Global']) . "</td>";
 
                             echo "</tr>";
                         }
