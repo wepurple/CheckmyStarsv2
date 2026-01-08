b2cd28e1a3668704943974dae76dc5be88f78d93
terence-git <temartinant@stpbb.org>
2026-01-08 09:21:21 +0100

    sonarqube ahh correction

--- models/crud/infoDossier.php
+++ models/crud/infoDossier.php
@@ -40,5 +40,3 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
     http_response_code(405);
     echo json_encode(["message" => "La méthode n'est pas autorisée"]);
 }
-
-?>
--- models/crud/supprimer.php
+++ models/crud/supprimer.php
@@ -29,11 +29,9 @@ if($_SERVER['REQUEST_METHOD'] == 'DELETE'){
     }
     else{
         http_response_code(503);
-        //echo json_encode($GLOBALS);
         echo json_encode(["message" => "Données invalides"]);
     }
 } else {
     http_response_code(405);
     echo json_encode(["message" => "La méthode n'est pas autorisée"]);
 }
-?>
