de2c9e787b81ee701806f63e6d9a2f206e8c671e
Pedro <pedro@mail.com>
2026-01-14 23:12:33 +0100

    bootstrap

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login - CheckMyStars</title>
 
-  <link rel="stylesheet" href="bootstrap 5.3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="bootstrap 5.3/css/bootstrap.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel="stylesheet" href="bootstrap 5.3/css/style.css">
   <script src="bootstrap 5.3/js/bootstrap.js"></script>
