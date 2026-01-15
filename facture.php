09ba415727792e518c5cfb11fff15a8464520804
Server <serv@mail.com>
2026-01-15 13:08:43 +0100

    navbar fix

--- facture.php
+++ facture.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION['ID'])) {
 }
 ?>
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
