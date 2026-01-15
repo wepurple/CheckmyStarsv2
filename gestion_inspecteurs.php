f61b4cd57020501ed584936cf25f9339e0086b3f
Server <serv@mail.com>
2026-01-15 16:03:44 +0100

    fontawesome

--- gestion_inspecteurs.php
+++ gestion_inspecteurs.php
@@ -20,11 +20,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Gestion inspecteurs - CheckMyStars</title>
 
-        <link rel="stylesheet" href="bootstrap 5.3/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+        <link rel="stylesheet" href="bootstrap 5.3/css/bootstrap.css">
+        <link rel="stylesheet" href="./fontawesome-5.15.4/css/all.css">
+        <link rel="icon" type="image/x-icon" href="pictures/logosm.png">
+        
         <script src="bootstrap 5.3/js/bootstrap.js"></script>
         <script src="js/search_inspecteurs.js"></script>
-        <link rel="icon" type="image/x-icon" href="pictures/logosm.png">
     </head>
 
     <body class="bg-secondary">
