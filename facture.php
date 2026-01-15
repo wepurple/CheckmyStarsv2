fdc556bee6bb0f1eb174ae75e6fd217763b220c3
Server <serv@mail.com>
2026-01-15 16:16:07 +0100

    facture css

--- facture.php
+++ facture.php
@@ -15,10 +15,10 @@ if (!isset($_SESSION['ID'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./fontawesome-7.1.0/css/all.css">
     <title>Gestion des Factures - CheckMyStars</title>
-    <link rel="stylesheet" href="bootstrap 5.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap 5.3/css/bootstrap.css">
     <link rel="stylesheet" href="bootstrap 5.3/css/facture.css">
 </head>
-<body>
+<body class="bg-secondary">
      <?php
             require_once("./includes/navbar.php");
         ?>
