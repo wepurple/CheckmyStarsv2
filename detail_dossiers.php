8d0eff8375ecf12c2350d1d1372bae3f1cb9c109
Kylian <kylebigot@stpbb.org>
2026-01-15 16:29:38 +0100

    merde

--- detail_dossiers.php
+++ detail_dossiers.php
@@ -95,7 +95,7 @@
                             ORDER BY B.Bien_ID DESC;";
                             
                             $stmt = $db->prepare($sql);
-                            $stmt->execute([':bienId' => $bienId]);
+                            $stmt->execute();
                             
                             if ($stmt->rowCount() > 0) {
                                 while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
