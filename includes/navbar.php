c90c115a9c60fdd8d80f5ef39f3e4321e268aa5f
Server <serv@mail.com>
2026-01-15 16:45:41 +0100

    ez

--- includes/navbar.php
+++ includes/navbar.php
@@ -39,7 +39,7 @@
 
             <hr>
 
-            <div class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <div class="navbar-nav ms-auto mb-2 mb-lg-0 text-light">
                 <span class="nav-text">Connecté<?php if(isset($_SESSION['Prenom']) && isset($_SESSION['Nom'])){ echo " en tant que " . $_SESSION['Prenom'] . " " . $_SESSION['Nom']; } ?></span>
             </div>
 
