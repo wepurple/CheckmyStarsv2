db41959015583abddf1bf55782ac5a5d2f895ab6
Server <serv@mail.com>
2026-01-15 16:42:44 +0100

    blanc

--- gestion_inspecteurs.php
+++ gestion_inspecteurs.php
@@ -158,7 +158,7 @@
                         <!-- modal footer -->
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
-                            <button type="button" class="btn btn-success"><i class="fa-solid fa-check mx-1" onclick="addUser()"></i>Ajouter</button>
+                            <button type="button" class="btn btn-success" onclick="addUser()"><i class="fa-solid fa-check mx-1"></i>Ajouter</button>
                         </div>
                     </div>                    
                 </div>
