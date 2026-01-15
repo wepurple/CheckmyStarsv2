1d054e5a1a8be68d385a2f83ea14174f27633138
DVZAngelNCTS <angelnctseuw@gmail.com>
2026-01-15 09:12:29 +0100

    test fix

--- critereBack.php
+++ critereBack.php
@@ -113,7 +113,7 @@ function getNumberCriteriaByStatusAndStar(PDO $pdo, int $star, string $status): 
                                 </br>
                                 <p class="card-text"><?= getNumberEstablishmentByStar($db, $x) ?> établissement à <?= $x ?> étoile</p>
                                 </br>
-                                <a href="critereBackStar.php?star=<?= $x ?>" class="btn btn-primary">
+                                <a href="critereBack<?= $x ?>Star.php" class="btn btn-primary">Accéder aux critères</a>
                                     Accéder aux critères
                                 </a>
                             </div>
