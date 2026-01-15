a954e5a9e0cf9fa7203e2aac884933a612bd37df
Kylian <kylebigot@stpbb.org>
2026-01-15 16:14:08 +0100

    let s go

--- detail_dossiers.php
+++ detail_dossiers.php
@@ -90,6 +90,7 @@
                             INNER JOIN donneurordre as d on b.Donneur_ID = d.Donneur_ID
                             INNER JOIN typeshebergements as t on b.TypeHebergement_ID = t.TypeHebergement_ID
                             INNER JOIN dossiers as do on u.Utilisateur_ID = do.Utilisateur_ID
+                            WHERE b.Bien_ID = do.Bien_ID
                             ORDER BY B.Bien_ID DESC;";
                             
                             $stmt = $db->prepare($sql);
