127163bdacaca54440fe6b3b0204f8f2f448ad22
Kylian <kylebigot@stpbb.org>
2026-01-16 15:59:02 +0100

    bah c bon dcp

--- front_dossier.php
+++ front_dossier.php
@@ -29,8 +29,8 @@
                 <div class="col-md 4">
                     <div id="Carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
                         <div class="carousel-inner">
-                            <div class="carousel-item">
-                                <img class="img_hotel" src="img\hotel_img1.jpg" class="d-block w-100" alt="Image 1">
+                            <div class="carousel-item active">
+                                <img src="img\hotel_img1.jpg" class="d-block w-100" alt="Image 1">
                             </div>
                             <div class="carousel-item">
                                 <img src="img\hotel_img2.jpg" class="d-block w-100" alt="Image 2">
