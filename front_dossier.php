4af9a166ef16376974e046bdf93ee0dcdf823273
Kylian <kylebigot@stpbb.org>
2026-01-16 14:38:30 +0100

    création du front hand pour les details du dossiers

---
+++ front_dossier.php
@@ -0,0 +1,23 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr" data-bs-theme="dark">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Dossiers- CheckMyStars</title>
+
+        <link rel="stylesheet" href="bootstrap 5.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="./fontawesome-7.1.0/css/all.css">
+        <script src="bootstrap 5.3/js/bootstrap.js"></script>
+        <script src="js/search_inspecteurs.js"></script>
+        <link rel="icon" type="image/x-icon" href="pictures/logosm.png">
+    </head>
+
+    <body class="bg-secondary">
+        <?php
+            require_once "./includes/navbar.php";
+        ?>
+    </body>
+</html>
