3af4171808266f2d8c9e344c8b13ba0c511c5403
Kylian <kylebigot@stpbb.org>
2026-01-16 15:24:22 +0100

    lz bon chemin en big W

--- front_dossier.php
+++ front_dossier.php
@@ -24,13 +24,13 @@
         <div id="Carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="../img/hotel1.jpg" class="d-block w-100" alt="Image 1">
+                    <img src="img\hotel_img1.jpg" class="d-block w-100" alt="Image 1">
                 </div>
-                <div class="carousel-item active">
-                    <img src="../img/hotel2.jpg" class="d-block w-100" alt="Image 2">
+                <div class="carousel-item">
+                    <img src="img\hotel_img2.jpg" class="d-block w-100" alt="Image 2">
                 </div>
-                <div class="carousel-item active">
-                    <img src="../img/hotel3.jpg" class="d-block w-100" alt="Image 3">
+                <div class="carousel-item">
+                    <img src="img\hotel_img3.jpg" class="d-block w-100" alt="Image 3">
                 </div>
             </div>
         </div>
