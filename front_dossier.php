cde0d8a74e8679e7e0f1e433001c34e2ba432f8b
Kylian <kylebigot@stpbb.org>
2026-01-16 15:18:27 +0100

    test du carousel bootstrap

--- front_dossier.php
+++ front_dossier.php
@@ -13,11 +13,26 @@
         <script src="bootstrap 5.3/js/bootstrap.js"></script>
         <script src="js/search_inspecteurs.js"></script>
         <link rel="icon" type="image/x-icon" href="pictures/logosm.png">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     </head>
 
     <body class="bg-secondary">
         <?php
             require_once "./includes/navbar.php";
         ?>
+        <div id="Carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="hotel1.jpg" class="d-block w-100" alt="Image 1">
+                </div>
+                <div class="carousel-item active">
+                    <img src="hotel2.jpg" class="d-block w-100" alt="Image 2">
+                </div>
+                <div class="carousel-item active">
+                    <img src="hotel3.jpg" class="d-block w-100" alt="Image 3">
+                </div>
+            </div>
+        </div>
     </body>
 </html>
