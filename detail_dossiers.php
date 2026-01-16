9c4a0a0974764f5cad28860625b3abbeed033beb
Kylian <kylebigot@stpbb.org>
2026-01-16 13:35:22 +0100

    manquement de :

--- detail_dossiers.php
+++ detail_dossiers.php
@@ -86,11 +86,11 @@
                             INNER JOIN donneurordre as d on b.Donneur_ID = d.Donneur_ID
                             INNER JOIN typeshebergements as t on b.TypeHebergement_ID = t.TypeHebergement_ID
                             INNER JOIN dossiers as do on u.Utilisateur_ID = do.Utilisateur_ID
-                            WHERE b.Bien_ID = doBienID
+                            WHERE b.Bien_ID = :doBienID
                             ORDER BY B.Bien_ID DESC";
                             
                             $stmt = $db->query($sql);
-                            $stmt->execute([':do.Bien_ID' => $doBienID]);
+                            $stmt->execute([':doBienID' => $doBienID]);
                             
                             if ($stmt->rowCount() > 0) {
                                 while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
