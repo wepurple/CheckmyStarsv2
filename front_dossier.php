1d4593819e6c393e103036ef86436def3bb404e3
Kylian <kylebigot@stpbb.org>
2026-01-16 16:23:24 +0100

    rounded

--- front_dossier.php
+++ front_dossier.php
@@ -25,7 +25,7 @@
             <div class="row justify-content-center">
                 <div class="col-md 4">
                 </div>
-                <div class="col-md 4 p-3">
+                <div class="col-md 4 p-3 rounded">
                     <div id="Carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
                         <div class="carousel-inner">
                             <div class="carousel-item active">
