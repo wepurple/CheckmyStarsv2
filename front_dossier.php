2502fcffe503e0941caab2686f13cf01c5c9fbe2
Kylian <kylebigot@stpbb.org>
2026-01-16 16:07:36 +0100

    style in html

--- front_dossier.php
+++ front_dossier.php
@@ -29,7 +29,7 @@
                     <div id="Carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
                         <div class="carousel-inner">
                             <div class="carousel-item active">
-                                <img src="img\hotel_img1.jpg" class="d-block w-100" alt="Image 1">
+                                <img src="img\hotel_img1.jpg" style="border-radius: 20px;" class="d-block w-100" alt="Image 1">
                             </div>
                             <div class="carousel-item">
                                 <img src="img\hotel_img2.jpg" class="d-block w-100" alt="Image 2">
