17e2a4ad674a227c0a78e77655429ddb67e79eae
Kylian <kylebigot@stpbb.org>
2026-01-21 13:07:11 +0100

    2 carousel

--- front_dossier.php
+++ front_dossier.php
@@ -39,6 +39,19 @@
                             </div>
                         </div>
                     </div>
+                    <div id="Carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="img\hotel_img1.jpg" style="border-radius: 20px;" class="d-block w-100" alt="Image 1">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="img\hotel_img2.jpg" style="border-radius: 20px;" class="d-block w-100" alt="Image 2">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="img\hotel_img3.jpg" style="border-radius: 20px;" class="d-block w-100" alt="Image 3">
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-md 4">
                 </div>
