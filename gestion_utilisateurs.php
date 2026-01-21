3af619aed82863effbed640d8cd04b66eb2f81e1
DVZAngelNCTS <angelnctseuw@gmail.com>
2026-01-21 13:15:18 +0100

    mw 15 to 10

--- gestion_utilisateurs.php
+++ gestion_utilisateurs.php
@@ -48,7 +48,7 @@ if (isset($_SESSION['Role']['Administrateur'])) {
 
                 <span class="input-group-text">Rechercher par</span>
 
-                <select class="form-select max-w-15" id="type">
+                <select class="form-select max-w-10" id="type">
                     <option selected value="1">ID</option>
                     <option value="2">Nom</option>
                     <option value="3">Prénom</option>
