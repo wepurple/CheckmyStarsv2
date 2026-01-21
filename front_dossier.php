6a7f969a403fe67dcae3658b8dddcfb166a03158
Kylian <kylebigot@stpbb.org>
2026-01-21 14:17:42 +0100

    test border

--- front_dossier.php
+++ front_dossier.php
@@ -70,7 +70,7 @@
                 </div>
             </div>
             <div class="row">
-                <div class="card-6 text-white bg-primary rounded shadow-md">
+                <div class="card-6 text-white border-radius rounded shadow-md">
                     <div class="card-body">
                         <p class="card-text">text</br>
                             a</br>
@@ -79,7 +79,7 @@
                         </p>
                     </div>
                 </div>
-                <div class="card-6 text-white bg-danger rounded shadow-md">
+                <div class="card-6 text-white border-radius rounded shadow-md">
                     <div class="card-body">
                         <p class="card-text">text</br>
                             a</br>
