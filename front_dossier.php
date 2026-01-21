86372cda5a9fb28d4123f770a2f2947943cecd28
Kylian <kylebigot@stpbb.org>
2026-01-21 13:12:15 +0100

    bim

--- front_dossier.php
+++ front_dossier.php
@@ -24,7 +24,9 @@
         <div class="container-sm">
             <div class="row justify-content-center">
                 <div class="col-md 4 p-3 rounded">
-                    <p>bambam</p>
+                    <div id="Carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
+                        <p>bambam</p>
+                    </div>
                 </div>
                 <div class="col-md 4 p-3 rounded">
                     <div id="Carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
