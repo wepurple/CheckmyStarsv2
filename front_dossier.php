5fcf7f2f103357790f7523b37ea7e5df205b93b2
Kylian <kylebigot@stpbb.org>
2026-01-21 13:57:38 +0100

    test petit shadow

--- front_dossier.php
+++ front_dossier.php
@@ -70,12 +70,13 @@
                 </div>
             </div>
             <div class="row">
-                <div class="card-6 text-white bg-primary rounded shadow-lg">
+                <div class="card-6 text-white bg-primary rounded shadow-sm">
                     <div class="card-body">
                         <p class="card-text">HAAAAAAAAAAAAAAAAAAAAAAAAA</p>
                     </div>
                 </div>
-                <div class="card-6 text-white bg-danger rounded shadow-lg">
+                <br>
+                <div class="card-6 text-white bg-danger rounded shadow-sm">
                     <div class="card-body">
                         <p class="card-text">HAAAAAAAAAAAAAAAAAAAAAAAAA</p>
                     </div>
