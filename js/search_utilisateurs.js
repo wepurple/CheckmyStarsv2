5dd46a92cfddb44b695030b7d34c4163027bc662
DVZAngelNCTS <angelnctseuw@gmail.com>
2026-01-21 14:40:46 +0100

    css bootstrap

--- js/search_utilisateurs.js
+++ js/search_utilisateurs.js
@@ -44,7 +44,7 @@ async function loadTable()
                 <td>${user.Societe_Nom || ''}</td>
                 <td class="text-end">
                     <button class="btn btn-secondary btn-sm me-2"> <i class="fa-solid fa-eye"></i> </button>
-                    <button class="btn btn-sm btn-warning">Edit</button>
+                    <button class="btn btn-sm btn-warning me-2">Edit</button>
                     <button class="btn btn-sm btn-danger">Delete</button>
                 </td>
             `;
