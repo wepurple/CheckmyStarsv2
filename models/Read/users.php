651577864d3a1e1f36f41a448b75d7b551ec3927
DVZAngelNCTS <angelnctseuw@gmail.com>
2026-01-21 15:10:38 +0100

    fix

--- models/Read/users.php
+++ models/Read/users.php
@@ -45,6 +45,14 @@
         }
 
         public function getAllUsers(){
+            $sql = "CALL Get_User_ID(".$id.");";
+            $query = $this->connexion->prepare($sql);
+            $query->execute();
+
+            return $query;
+        }
+
+        public function getUserById($Id){
             $sql = "CALL Get_User;";
             $query = $this->connexion->prepare($sql);
             $query->execute();
