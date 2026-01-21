a0fe66cafb7cd81b4a72ca00b47282c3e5476d2a
Kylian <kylebigot@stpbb.org>
2026-01-21 16:25:32 +0100

    le deuxieme

--- front_dossier.php
+++ front_dossier.php
@@ -79,6 +79,15 @@
                         </div>
                     </div>
                 </div>
+                <div class="col-md rounded">
+                    <div class="card-6 text-white bg-dark rounded shadow-md border" >
+                        <div class="card-body">
+                            <div class="card-title h3">Evolution</div>
+                                <p class="card-text">Texte</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </body>
