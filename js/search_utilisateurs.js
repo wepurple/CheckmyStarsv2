71f7ba1f7bf88c5ace6313750cb001a4ab6d5aff
DVZAngelNCTS <angelnctseuw@gmail.com>
2026-01-21 14:18:15 +0100

    fix

--- js/search_utilisateurs.js
+++ js/search_utilisateurs.js
@@ -23,14 +23,27 @@ async function loadTable()
 {
     var users = await getAllusers()
     var tab = document.getElementById("table-body");
+    tab.innerHTML = ""; // Vider le tableau
 
     for (var i = 0; i < users.length; i++)
     {
-        console.log(users[i].Utilisateur_Nom);
-
-        e=tab.lastElementChild
-        e.appendChild(document.createElement("th"))
-        e=tab.lastElementChild
-        e.appendChild(document.createElement('td'))
+        var user = users[i];
+        
+        var tr = document.createElement("tr");
+        
+        tr.innerHTML = `
+            <td>${user.Utilisateur_ID || user.IdPersonne || ''}</td>
+            <td>${user.Nom || ''}</td>
+            <td>${user.Prenom || ''}</td>
+            <td>${user.Email || ''}</td>
+            <td>Role</td>
+            <td>${user.Societe || ''}</td>
+            <td class="text-end">
+                <button class="btn btn-sm btn-warning">Edit</button>
+                <button class="btn btn-sm btn-danger">Delete</button>
+            </td>
+        `;
+        
+        tab.appendChild(tr);
     }
 }
