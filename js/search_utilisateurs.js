2e7cc489e36d6a6cc0cfd8f6649072f92466ea61
DVZAngelNCTS <angelnctseuw@gmail.com>
2026-01-21 15:32:01 +0100

    fix id

--- js/search_utilisateurs.js
+++ js/search_utilisateurs.js
@@ -77,7 +77,7 @@ async function loadTable()
                 <td>${roleUser}</td>
                 <td>${user.Societe_Nom || ''}</td>
                 <td class="text-end">
-                    <button class="btn btn-secondary btn-sm me-2" onclick="showUserInfoModal(1)"> <i class="fa-solid fa-eye"></i> </button>
+                    <button class="btn btn-secondary btn-sm me-2" onclick="showUserInfoModal(${i})"> <i class="fa-solid fa-eye"></i> </button>
                     <button class="btn btn-sm btn-warning me-2">Edit</button>
                     <button class="btn btn-sm btn-danger">Delete</button>
                 </td>
