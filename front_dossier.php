e97f4935b7d6b19d57697deb867c60f5141d13b1
Kylian <kylebigot@stpbb.org>
2026-01-21 13:12:50 +0100

    2 eme test

--- front_dossier.php
+++ front_dossier.php
@@ -25,7 +25,9 @@
             <div class="row justify-content-center">
                 <div class="col-md 4 p-3 rounded">
                     <div id="Carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
-                        <p>bambam</p>
+                        <div class="carousel-inner">
+                            <p>bambam</p>
+                        </div>
                     </div>
                 </div>
                 <div class="col-md 4 p-3 rounded">
