6bb458a337cf271c4088f63e8f1484eaadcacc5b
Server <serv@mail.com>
2026-01-22 16:40:23 +0100

    sql 3

--- js/profil.js
+++ js/profil.js
@@ -1,3 +1,32 @@
+const ids = [
+    "nom",
+    "prenom",
+    "civilite",
+    "mail",
+    "tel",
+    "societe",
+    "numRue",
+    "nomRue",
+    "complement",
+    "codePost",
+    "ville",
+    "pays"
+]
+const elements = [
+    "Utilisateur_Nom",
+    "Utilisateur_Prenom",
+    "Utilisateur_Civilite",
+    "Utilisateur_Mail",
+    "Utilisateur_Telephone",
+    "Utilisateur_Societe",
+    "AdressePostale_NumeroRue",
+    "AdressePostale_NomRue",
+    "AdressePostale_Complement",
+    "AdressePostale_CodePostal",
+    "AdressePostale_Ville",
+    "AdressePostale_Pays"
+]
+    
 async function getInfos() {
     const url = "models/crud/myInfo.php"
     const response = await fetch(url, {
@@ -13,42 +42,19 @@ async function getInfos() {
 
 async function updateInfos(){
     const list = await getInfos()
-    console.log(list)
-    const ids = [
-        "nom",
-        "prenom",
-        "civilite",
-        "mail",
-        "tel",
-        "societe",
-        "numRue",
-        "nomRue",
-        "complement",
-        "codePost",
-        "ville",
-        "pays"
-    ] 
-    const elements = [
-        "Utilisateur_Nom",
-        "Utilisateur_Prenom",
-        "Utilisateur_Civilite",
-        "Utilisateur_Mail",
-        "Utilisateur_Telephone",
-        "Utilisateur_Societe",
-        "AdressePostale_NumeroRue",
-        "AdressePostale_NomRue",
-        "AdressePostale_Complement",
-        "AdressePostale_CodePostal",
-        "AdressePostale_Ville",
-        "AdressePostale_Pays"
-    ]
+    //console.log(list)
 
-    for (i = 0 ; i<ids.length ; i++){        
-        document.getElementById(ids[i]).textContent = list[elements[i]]
+    for (let i = 0 ; i<ids.length ; i++){        
+        document.getElementById(ids[i]).value = list[elements[i]]
     }
 }
 
-
+function edit(){
+    for (let j = 0 ; j<ids.length ; j++){
+        document.getElementById(ids[j]).removeAttribute("disabled")
+    }
+    document.getElementById("valider").removeAttribute("disabled")
+}
 
 document.addEventListener("DOMContentLoaded", async function() {//quand la page est chargée
     updateInfos()
