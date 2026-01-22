e5b0d8733186bedcd19f8943c2aa5ab696b497d2
Kylian <kylebigot@stpbb.org>
2026-01-22 09:54:39 +0100

    close button footer

--- front_dossier.php
+++ front_dossier.php
@@ -123,9 +123,12 @@
                 <div class="modal-content">
                     <div header class="header">
                         <h5 class="modal-title">Informations</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         <div class="modal-body">
                             <p>Contenu du modal...</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
                     </div>
                 </div>
             </div>
