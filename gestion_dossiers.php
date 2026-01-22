f4442afc429326c1f10905edc95ffb91bcc84124
Kylian <kylebigot@stpbb.org>
2026-01-22 11:47:11 +0100

    on vas voir mdr

--- gestion_dossiers.php
+++ gestion_dossiers.php
@@ -141,7 +141,24 @@
                         echo "<tr><td colspan='7' class='text-center text-danger'>Erreur de connexion à la base de données</td></tr>";
                     } else {
                         try {
-                            $sql = "CALL Get_Dossier;";
+                            $sql = "SELECT 
+                                d.Dossier_ID, 
+                                d.DOSSIER_NUMERO, 
+                                t.TypeHebergement_Nom, 
+                                u.Utilisateur_Nom, 
+                                u.Utilisateur_Prenom, 
+                                a.AdressePostale_NumeroRue, 
+                                a.AdressePostale_NomRue, 
+                                a.AdressePostale_CodePostal, 
+                                a.AdressePostale_Ville, 
+                                a.AdressePostale_Pays, 
+                                d.status 
+                            FROM dossiers AS d 
+                            INNER JOIN utilisateurs AS u ON d.Proprietaire_Id = u.Utilisateur_ID 
+                            INNER JOIN biens AS b ON b.Bien_ID = d.Bien_ID
+                            INNER JOIN adressespostales AS a ON a.AdressePostale_ID = b.AdressePostale_ID 
+                            INNER JOIN typeshebergements AS t ON t.TypeHebergement_ID = b.TypeHebergement_ID
+                            ORDER BY d.Dossier_ID DESC";
                             $stmt = $db->prepare($sql);
                             $stmt->execute();
                             
