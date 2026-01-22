1569d4855311ea40cc01506acea38861a991047d
DVZAngelNCTS <angelnctseuw@gmail.com>
2026-01-22 11:34:29 +0100

    fix

--- js/search_utilisateurs.js
+++ js/search_utilisateurs.js
@@ -249,6 +249,10 @@ async function loadTable()
             {
                 roleUser = "Inspecteur";
             }
+            else if (user && user.donneurordre == 1)
+            {
+                roleUser = "Donneur d'ordre";
+            }
             else if (user && user.proprietaire == 1)
             {
                 roleUser = "Proprietaire";
