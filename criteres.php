55c9bfec84c932dec4612203367ba9581d2d1337
Midoux <noe@mail.org>
2026-01-22 08:35:56 +0100

    ajout dossiers des critères inspecteurs

---
+++ criteres.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire de Saisie des Critères de Classement</title>
+</head>
+<body>
+    <div>
+        <h1>Parametres du logement</h1>
+
+        <div>
+            <p>Nombre d'étoile visé</p>
+        </div>
+        
+    </div>
+
+    <div>
+
+    </div>
+
+    <div class="">
+
+    </div>
+    
+</body>
+</html>
