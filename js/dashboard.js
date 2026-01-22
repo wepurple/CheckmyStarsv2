8a1fdc56d4852cabef2f8d46687a5278b9762751
terence-git <temartinant@stpbb.org>
2026-01-22 15:32:04 +0100

    tzst

--- js/dashboard.js
+++ js/dashboard.js
@@ -33,6 +33,18 @@ function remplirTab(liste) {//remplit le tablo
 
             e.appendChild(document.createElement('td'))
             e.lastElementChild.textContent = liste[i]["Utilisateur_ID"]
+
+            e.appendChild(document.createElement('td'))
+            e.lastElementChild.textContent = liste[i]["Utilisateur_ID"]
+
+            e.appendChild(document.createElement('td'))
+            e.lastElementChild.textContent = liste[i]["Utilisateur_ID"]
+
+            e.appendChild(document.createElement('td'))
+            e.lastElementChild.textContent = liste[i]["Utilisateur_ID"]
+            
+            e.appendChild(document.createElement('td'))
+            e.lastElementChild.textContent = liste[i]["Utilisateur_ID"]
         }
     }
 }
