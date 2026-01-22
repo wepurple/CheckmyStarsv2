982d1a2c9bade14b94d7253f151c36f92567c635
Kylian <kylebigot@stpbb.org>
2026-01-22 09:51:06 +0100

    test

--- front_dossier.php
+++ front_dossier.php
@@ -74,13 +74,13 @@
             <div class="row">
                 <div class="col-md rounded">
                     <div class="card text-white rounded shadow-md border" style="background-color: #464646 !important;">
-                            <button type="button" class="btn" data-bs-toggle="info modal">Voir le devis</button>
+                            <button type="button" class="btn" data-bs-toggle="info_modal" data-bs-target="#info_modal">Voir le devis</button>
                         </div>
                     </div>
                 </div>
                 <div class="col-md rounded">
                     <div class="card text-white rounded shadow-md border" style="background-color: #2b2b2b !important;">
-                            <button type="button" class="btn" data-bs-toggle="info modal">Voir la facture</button>
+                            <button type="button" class="btn" data-bs-toggle="info_modal">Voir la facture</button>
                         </div>
                     </div>
                 </div>
@@ -90,13 +90,13 @@
             <div class="row">
                 <div class="col-md rounded">
                     <div class="card text-white rounded shadow-md border" style="background-color: #464646 !important;">
-                            <button type="button" class="btn" data-bs-toggle="info modal">Voir l'evolution</button>
+                            <button type="button" class="btn" data-bs-toggle="info_modal">Voir l'evaluation</button>
                         </div>
                     </div>
                 </div>
                 <div class="col-md rounded">
                     <div class="card text-white rounded shadow-md border" style="background-color: #2b2b2b !important;">
-                            <button type="button" class="btn" data-bs-toggle="info modal">Voir l'adresse</button>
+                            <button type="button" class="btn" data-bs-toggle="info_modal">Voir l'adresse</button>
                         </div>
                     </div>
                 </div>
@@ -106,19 +106,19 @@
             <div class="row">
                 <div class="col-md rounded">
                     <div class="card text-white rounded shadow-md border" style="background-color: #464646 !important;">
-                            <button type="button" class="btn" data-bs-toggle="info modal">Voir la date du RDV</button>
+                            <button type="button" class="btn" data-bs-toggle="info_modal">Voir la date du RDV</button>
                         </div>
                     </div>
                 </div>
                 <div class="col-md rounded">
                     <div class="card text-white rounded shadow-md border" style="background-color: #2b2b2b !important;">
-                            <button type="button" class="btn" data-bs-toggle="info modal">Voir l'etat du dossier</button>
+                            <button type="button" class="btn" data-bs-toggle="info_modal">Voir l'etat du dossier</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="modal" id="info modal" tabindex="-1">
+        <div class="modal" id="info_modal" tabindex="-1">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div header class="header">
