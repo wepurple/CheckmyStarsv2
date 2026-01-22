3845123b393b7830393a132da5fad753582662d9
DVZAngelNCTS <angelnctseuw@gmail.com>
2026-01-22 13:12:14 +0100

    fix order

--- models/Update/users.php
+++ models/Update/users.php
@@ -15,32 +15,29 @@
 
         public function updateUserById($nom, $prenom, $email, $civilite, $societe_id, $role_id, $telephone, $num_rue, $nom_rue, $complement, $code_postal, $ville, $pays, $id)
         {
-            // 14 placeholders pour 14 paramètres
             $sql = "SELECT Update_User(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) AS result";
             $query = $this->connexion->prepare($sql);
             
-            // ORDRE CORRECT : Nom, Prenom, Societe, Mail, Genre, Telephone, NumRue, Complement, CP, Adresse, Ville, Pays, Role, ID
+            // ORDRE CORRECT selon la fonction SQL
             $query->execute([
                 $nom,           // 1. Nom
                 $prenom,        // 2. Prenom
-                $societe_id,    // 3. Societe
-                $email,         // 4. Mail ← CORRIGÉ
-                $civilite,      // 5. Genre ← CORRIGÉ
+                $email,         // 3. Mail
+                $civilite,      // 4. Genre
+                $societe_id,    // 5. Societe
                 $telephone,     // 6. Telephone
                 $num_rue,       // 7. NumRue
-                $complement,    // 8. Complement
-                $code_postal,   // 9. CP
-                $nom_rue,       // 10. Adresse
+                $nom_rue,       // 8. Adresse
+                $complement,    // 9. Complement
+                $code_postal,   // 10. CP
                 $ville,         // 11. Ville
                 $pays,          // 12. Pays
-                $role_id,       // 13. Role
-                $id             // 14. ID
+                $id,            // 13. ID
+                $role_id        // 14. Role
             ]);
             
             return $query;
         }
-
-
     }
     
     try {
