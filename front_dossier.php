09d4f553448f875339a35ca97626eecc3b65208c
Kylian <kylebigot@stpbb.org>
2026-01-22 16:09:39 +0100

    alors ?

--- front_dossier.php
+++ front_dossier.php
@@ -140,7 +140,7 @@
 
                                         if ($stmt->rowCount() > 0) {
                                             while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                                                echo "<td>" . htmlspecialchars($row['AdressePostale_NumeroRue']) . " " . htmlspecialchars($row['AdressePostale_NomRue']) . "</td>" . "</br>" ;
+                                                echo "<td>" . htmlspecialchars($row['AdressePostale_NumeroRue']) . " " . htmlspecialchars($row['AdressePostale_NomRue']) . "</td>" . " " . htmlspecialchars($row['AdressePostale_Complement']) . "</td>" . "</br>" ;
                                                 echo "<td>" . htmlspecialchars($row['AdressePostale_CodePostal']) . "</td>" . "</br>" ;
                                                 echo "<td>" . htmlspecialchars($row['AdressePostale_Ville']) . "</td>" . "</br>" ;
                                                 echo "<td>" . htmlspecialchars($row['AdressePostale_Pays']) . "</td>" . "</br>";
