e6fdb8e627693d79edd4c26574fa9b5f7ac5d97e
Kylian <kylebigot@stpbb.org>
2026-01-22 08:46:54 +0100

    c'est ok ou pas dcp ?

--- front_dossier.php
+++ front_dossier.php
@@ -79,8 +79,24 @@
                     </div>
                 </div>
                 <div class="col-md rounded">
-                    <div class="card text-white rounded shadow-md border" style="background-color: #464646 !important;">
+                    <div class="card text-white rounded shadow-md border" style="background-color: #2b2b2b !important;">
                             <button type="button" class="btn">Voir la facture</button>
+                        </div>
+                    </div>
+                    <div class="card text-white rounded shadow-md border" style="background-color: #2b2b2b !important;">
+                            <button type="button" class="btn">Voir l'evolution</button>
+                        </div>
+                    </div>
+                    <div class="card text-white rounded shadow-md border" style="background-color: #2b2b2b !important;">
+                            <button type="button" class="btn">Voir l'adresse</button>
+                        </div>
+                    </div>
+                    <div class="card text-white rounded shadow-md border" style="background-color: #2b2b2b !important;">
+                            <button type="button" class="btn">Voir la date du RDV</button>
+                        </div>
+                    </div>
+                    <div class="card text-white rounded shadow-md border" style="background-color: #2b2b2b !important;">
+                            <button type="button" class="btn">Voir l'etat du dossier</button>
                         </div>
                     </div>
                 </div>
