906e23f740c5fd68d5c4f5f6e65d72babe26eadc
terence-git <temartinant@stpbb.org>
2026-01-22 13:48:55 +0100

    zsdfghn

--- js/dashboard.js
+++ js/dashboard.js
@@ -7,17 +7,21 @@ async function getInfos() {
         }
     })
 
-    const result = JSON.parse(await response.text())["utilisateur"][0]
+    const result = JSON.parse(await response.text())["utilisateur"]
     return result
 }
 
 async function viderTab() {//vide le tablo
-    tab = document.getElementById("table-body")
+    tab = document.getElementById("tabloBody")
     child = tab.lastElementChild
     while (child){
         tab.removeChild(child)
         child = tab.lastElementChild
     }
+}
+
+async function remplirTab() {//remplit le tablo
+    tab = document.getElementById("tabloBody")
 }
 
 document.addEventListener("DOMContentLoaded", async function() {//quand la page est chargée
