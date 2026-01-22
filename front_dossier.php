2c3e6520092f5034d6f82dcc130542cd2e53790a
Kylian <kylebigot@stpbb.org>
2026-01-22 08:25:38 +0100

    test button

--- front_dossier.php
+++ front_dossier.php
@@ -85,7 +85,7 @@
                     <div class="card text-white rounded shadow-md border" style="background-color: #464646 !important;">
                         <div class="card-body">
                             <div class="card-title h3">Devis</div>
-                                <p class="card-text">Texte Dynamique</p>
+                                <button type="button" class="btn-close" aria-label="Close">Texte Dynamique</button>
                             </div>
                         </div>
                     </div>
