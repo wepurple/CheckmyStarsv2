745137b9be431f3fc17133ab3d4ab92492e57d5d
Moliere <kimoliere@stpbb.org>
2026-01-23 15:38:42 +0100

    test

--- gestion_dossiers.php
+++ gestion_dossiers.php
@@ -147,6 +147,8 @@
                         } elseif (isset($_SESSION['Role']['Inspecteur']) && $_SESSION['Role']['Inspecteur']) {
                             $inspecteurID = $_SESSION['Utilisateur_ID'];
                             $sql = "CALL Get_Dossier_Inspecteur($inspecteurID);";
+                            
+                            var_dump($inspecteurID);
                         }
                             $stmt = $db->prepare($sql);
                             $stmt->execute();
