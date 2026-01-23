be0a4ae70b46b678d7ecc4ad310955e22278430d
Kylian <kylebigot@stpbb.org>
2026-01-23 15:51:35 +0100

    et la ?

--- front_dossier.php
+++ front_dossier.php
@@ -195,7 +195,7 @@
                                                 $statusClass = $row['status'] == 1 ? 'bg-success' : 'bg-warning text-dark';
                                                 echo "<h5> Le dossier est actuellement : </h5><td><span class='badge $statusClass'>$statusText</span></td></br>";
                                                 echo "<h5> Assigné à </h5>";
-                                                echo "<td>" . htmlspecialchars($row['Utilisateurs_Nom']) . "</td>" "</br>"  " " ;
+                                                echo "<td>" . htmlspecialchars($row['Utilisateurs_Nom']) . "</td>" . "</br>" . " " ;
                                                 echo "<td>" . htmlspecialchars($row['Utilisateurs_Prenom']) . "</td>";
                                                 echo "<td>" . htmlspecialchars($row['Utilisateurs_Mail']) . "</td>" "</br>";
                                                 echo "<td>" . htmlspecialchars($row['Utilisateurs_Telephone']) . "</td>" "</br>";
