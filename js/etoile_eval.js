3b26c26ed460f20aa43167b2871ee1ec7ac4eedc
Kylian <kylebigot@stpbb.org>
2026-01-28 16:36:33 +0100

    c'est ok ?

--- js/etoile_eval.js
+++ js/etoile_eval.js
@@ -1,10 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const liste = document.getElementById('selectEtoiles');
+const liste = document.getElementById('selectEtoiles');
 
-    liste.addEventListener('change', function() {
-        
-        const etoiles = this.value 
-        console.log(etoiles);
+liste.addEventListener('change', function() {
+    
+    const etoiles = this.value 
+    console.log(etoiles);
 
-    });
 });
