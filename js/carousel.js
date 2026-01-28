a6ace981a7065fb9b4354ace6d940631f186af2f
Kylian <kylebigot@stpbb.org>
2026-01-28 14:45:07 +0100

    bim

--- js/carousel.js
+++ js/carousel.js
@@ -1,23 +1,26 @@
-const carousel1 = new bootstrap.Carousel(document.getAnimationsById('Carousel1'), {
-    interval: false
+document.addEventListener('DOMContentLoaded', function() {
+
+    const carousel1 = new bootstrap.Carousel(document.getAnimationsById('Carousel1'), {
+        interval: false
+    });
+
+    const carousel2 = new bootstrap.Carousel(document.getAnimationsById('Carousel2'), {
+        interval: false
+    });
+
+    const carousel3 = new bootstrap.Carousel(document.getElementById('Carousel3'), {
+        interval: false
+    });
+
+    document.getElementById('prevBut').addEventListener('click'), function() {
+        carousel1.prev();
+        carousel2.prev();
+        carousel3.prev();
+    }
+
+    document.getElementById('nextBut').addEventListener('click'), function() {
+        carousel1.next();
+        carousel2.next();
+        carousel3.next();
+    }
 });
-
-const carousel2 = new bootstrap.Carousel(document.getAnimationsById('Carousel2'), {
-    interval: false
-});
-
-const carousel3 = new bootstrap.Carousel(document.getElementById('Carousel3'), {
-    interval: false
-});
-
-document.getElementById('prevBut').addEventListener('click'), function() {
-    carousel1.prev();
-    carousel2.prev();
-    carousel3.prev();
-}
-
-document.getElementById('nextBut').addEventListener('click'), function() {
-    carousel1.next();
-    carousel2.next();
-    carousel3.next();
-}
