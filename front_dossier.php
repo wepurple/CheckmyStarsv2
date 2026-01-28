4196ca819a6d3f5aba9a54030dada7d689851f8f
Kylian <kylebigot@stpbb.org>
2026-01-28 14:05:30 +0100

    test

--- front_dossier.php
+++ front_dossier.php
@@ -59,38 +59,6 @@
                         <span class="visually-hidden">Next</span>
                     </button>
                 </div>
-
-                
-                <div class="col-md 4 p-3 rounded">
-                    <div id="Carousel" class="carousel slide"  data-bs-ride="carousel" data-bs-interval="2000" data-bs-pause="false">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="img\hotel_img1.jpg" style="border-radius: 20px;" class="d-block w-100" alt="Image 1">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="img\hotel_img2.jpg" style="border-radius: 20px;" class="d-block w-100" alt="Image 2">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="img\hotel_img3.jpg" style="border-radius: 20px;" class="d-block w-100" alt="Image 3">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md 4 p-3 rounded">
-                    <div id="Carousel" class="carousel slide"  data-bs-ride="carousel" data-bs-interval="2000" data-bs-pause="false">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="img\hotel_img2.jpg" style="border-radius: 20px;" class="d-block w-100" alt="Image 2">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="img\hotel_img3.jpg" style="border-radius: 20px;" class="d-block w-100" alt="Image 3">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="img\hotel_img1.jpg" style="border-radius: 20px;" class="d-block w-100" alt="Image 1">
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
         <div class="container">
