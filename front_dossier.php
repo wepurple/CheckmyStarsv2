a81d69ee9aaaad1e7ac9243463c22d70de18d925
Kylian <kylebigot@stpbb.org>
2026-01-28 15:38:50 +0100

    test

--- front_dossier.php
+++ front_dossier.php
@@ -41,7 +41,7 @@
                 <div class="col-md 4 p-3 rounded">
                     <div id="Carousel" class="carousel slide"  data-bs-ride="carousel" data-bs-interval="2000" data-bs-pause="false">
                         <div class="carousel-inner">
-                            <div class="carousel-item">
+                            <div class="carousel-item active">
                                 <img src="img\hotel_img3.jpg" style="border-radius: 20px;" class="d-block w-100" alt="Image 3">
                             </div>
                             <div class="carousel-item">
@@ -51,16 +51,12 @@
                                 <img src="img\hotel_img2.jpg" style="border-radius: 20px;" class="d-block w-100" alt="Image 2">
                             </div>
                         </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#Carousel" >
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
                     </div>
                 </div>
                 <div class="col-md 4 p-3 rounded">
                     <div id="Carousel" class="carousel slide"  data-bs-ride="carousel" data-bs-interval="2000" data-bs-pause="false">
                         <div class="carousel-inner">
-                            <div class="carousel-item">
+                            <div class="carousel-item active">
                                 <img src="img\hotel_img1.jpg" style="border-radius: 20px;" class="d-block w-100" alt="Image 1">
                             </div>
                             <div class="carousel-item">
@@ -75,7 +71,7 @@
                 <div class="col-md 4 p-3 rounded">
                     <div id="Carousel" class="carousel slide"  data-bs-ride="carousel" data-bs-interval="2000" data-bs-pause="false">
                         <div class="carousel-inner">
-                            <div class="carousel-item">
+                            <div class="carousel-item active">
                                 <img src="img\hotel_img2.jpg" style="border-radius: 20px;" class="d-block w-100" alt="Image 2">
                             </div>
                             <div class="carousel-item">
