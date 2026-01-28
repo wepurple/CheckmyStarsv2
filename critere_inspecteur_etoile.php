87f6f4dbc2b550dc0f5cbe4e33b943bde87aa3f0
Kylian <kylebigot@stpbb.org>
2026-01-28 16:10:07 +0100

    change dossierId to id

--- critere_inspecteur_etoile.php
+++ critere_inspecteur_etoile.php
@@ -38,7 +38,7 @@
         <div>
             <div>
                 <div class="d-flex align-items-center gap-3 p-3">
-                    <a href="front_dossier.php?id=<?php echo $dossierId; ?>" class="btn btn-light rounded-circle shadow-sm d-flex align-items-center justify-content-center" style="width: 45px; height: 45px;">
+                    <a href="front_dossier.php?id=<?php echo $id; ?>" class="btn btn-light rounded-circle shadow-sm d-flex align-items-center justify-content-center" style="width: 45px; height: 45px;">
                         <i class="bi bi-arrow-left-short fs-3 text-dark"></i>
                     </a>
 
