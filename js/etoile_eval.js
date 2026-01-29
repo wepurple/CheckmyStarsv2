0143b9a6669487896c5e967825348890a5edba30
Kylian <kylebigot@stpbb.org>
2026-01-29 10:04:38 +0100

    fix ou 0

--- js/etoile_eval.js
+++ js/etoile_eval.js
@@ -156,7 +156,7 @@ let total = 0;
 
 function pointsTotal() {
         for (let i =0; i < Nb_case_max; i++) {
-            const element = document.getElementById(`case-${i}`).value;
+            const element = document.getElementById(`case-${i}`).value || 0;
             console.log('Element ', i, ': ', element);
 
             total += (element);
