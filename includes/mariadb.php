2af8ea8e661d362d1424e5fab3f5bf7d56fb1300
Moliere <kimoliere@stpbb.org>
2026-01-29 13:18:43 +0100

    facture

--- includes/mariadb.php
+++ includes/mariadb.php
@@ -20,7 +20,7 @@ class Database {
                     PDO::ATTR_EMULATE_PREPARES => false
                 ]
             );
-        } catch (PDOException $e) {d
+        } catch (PDOException $e) {
             $this->connexion = [
                 0 => false,
                 1 => mb_convert_encoding($e->getMessage(), "UTF-8")
