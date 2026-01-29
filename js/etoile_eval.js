68ca19b68b5307c3a2af7a23298c94ecfbd8a375
Kylian <kylebigot@stpbb.org>
2026-01-29 08:30:45 +0100

    test

--- js/etoile_eval.js
+++ js/etoile_eval.js
@@ -3,24 +3,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let allData = [];
 
-    fetch(`models/crud/getCriteresByEtoiles.php?star=${etoiles}`)
-    .then(response => {
-        console.log('Reponse fetch; ', response);
-        return response.json();
-    })
-    .then(data => {
-        console.log("Données recues: ", data);
-        allData = data;
-        displayDara(data);
-        setupFilters(data);
-    })
-    .catch(error => console.log("Erreur fetch : ", error));
-
     liste.addEventListener('change', function() {
         
         let etoiles = this.value 
         return etoiles;
+
     });
+
+    fetch(`models/crud/getCriteresByEtoiles.php?star=${etoiles}`)
+        .then(response => {
+            console.log('Reponse fetch; ', response);
+            return response.json();
+        })
+        .then(data => {
+            console.log("Données recues: ", data);
+            allData = data;
+            displayDara(data);
+            setupFilters(data);
+        })
+        .catch(error => console.log("Erreur fetch : ", error));
 
     console.log(etoiles);
 
