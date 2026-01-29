549a3a6d8a936f68d0e14aac4c60bc06453f0bde
Kylian <kylebigot@stpbb.org>
2026-01-29 08:25:38 +0100

    test

--- js/etoile_eval.js
+++ js/etoile_eval.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const liste = document.getElementById('selectEtoiles');
 
+    let allData = [];
+
+    fetch(`models/crud/getCriteresByEtoiles.php?star=${etoiles}`)
+
     liste.addEventListener('change', function() {
         
         const etoiles = this.value 
-        console.log(etoiles);
         return etoiles;
     });
+
+    console.log(etoiles);
+
+
 });
