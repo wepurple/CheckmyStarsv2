be9acaa7f82409a7b943011c3abbb2f2f60ad36d
Kylian <kylebigot@stpbb.org>
2026-01-29 15:37:06 +0100

    test

--- js/etoile_eval.js
+++ js/etoile_eval.js
@@ -155,7 +155,10 @@ let points_ONC = 0;
 let total = 0;
 
 function pointsTotal() {
-    total = 0;
+    points_X = 0;
+    points_O = 0;
+    points_NA = 0;
+    points_ONC = 0;
     for (let j =0; j < i; j++) {
         const checkbox = document.getElementById(`checkbox-${j}`);
         const status = document.getElementById(`statut-${j}`);
@@ -167,10 +170,22 @@ function pointsTotal() {
             if (status.textContent === "O") {
                 points_O += points;
             }
+            else if (status.textContent === "X") {
+                points_X += points;
+            }
+            else if (status.textContent === "NA") {
+                points_NA += points;
+            }
+            else if (status.textContent === "ONC") {
+                points_ONC += points;
+            }
         }
     }
     console.log('Total:', Number(total));
-    console.log('Points O:', points_O);
+    console.log('Points O :', points_O);
+    console.log('Points X :', points_X);
+    console.log('Points NA :', points_NA);
+    console.log('Points ONC :', points_ONC);
 }
 
 
