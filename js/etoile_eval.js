92f62e027e7713f0641181b01e7e143e763a114f
Kylian <kylebigot@stpbb.org>
2026-01-29 10:02:33 +0100

    caca supreme

--- js/etoile_eval.js
+++ js/etoile_eval.js
@@ -156,9 +156,9 @@ let total = 0;
 
 function pointsTotal() {
         for (let i =0; i < Nb_case_max; i++) {
-            const element = document.getElementById(`case-${i}`);
+            const element = document.getElementById(`case-${i}`).value;
 
-            total += (element).value;
+            total += (element);
         }
         console.log('Total:', total);
     }
