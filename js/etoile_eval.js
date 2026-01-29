d65495dac8fe54611eccc6ea7d34af22335fdf7b
Kylian <kylebigot@stpbb.org>
2026-01-29 08:47:36 +0100

    test

--- js/etoile_eval.js
+++ js/etoile_eval.js
@@ -20,6 +20,51 @@ document.addEventListener('DOMContentLoaded', function() {
             })
             .catch(error => console.log("Erreur fetch : ", error));
 
+        function displayData(data) {
+            const tbody = document.getElementById('table-body');
+
+            if(!tbody) {
+                console.error("Erreur: table-body introuvable");
+                return;
+            }
+
+            tbody.innerHTML = "";
+
+            data.forEach(critere => {
+                const row = tbody.insertRow();
+                row.innerHTML = `
+                <td>${critere.Critere_ID}</td>
+                <td>
+                    <div class="d-flex align-items-center gap-2">
+                        <span class="flex-grow-1">
+                        ${critere.Critere_description}
+                        </span>
+
+                        <div class="btn-group btn-group-sm ms-auto" role="group" aria-label="Actions">
+                            <div class="pe-3">
+                                <button type="button" class="btn btn-outline-primary btn-edit" 
+                                    data-bs-toggle="modal" 
+                                    data-bs-target="#editModal"
+                                    data-id="${critere.Critere_ID}"
+                                    data-description="${escapeHtml(critere.Critere_description)}"
+                                    data-statut="${critere.Critere_statut}"
+                                    data-points="${critere.Critere_points || ''}">
+                                    Modifier
+                                </button>
+                            </div>
+                            <div>
+                                <button type="button" class="btn btn-outline-danger btn-delete"
+                                    data-id="${critere.Critere_ID}">
+                                    Supprimer
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </td>
+                `;
+            });
+        }
+
         console.log(etoiles);
 
     return etoiles;
