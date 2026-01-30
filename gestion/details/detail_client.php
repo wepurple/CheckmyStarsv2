9fcdee7c85a671184f6c626db4a6403a6672aef5
Kylian <kylebigot@stpbb.org>
2026-01-30 15:46:35 +0100

    fixation beton

--- gestion/details/detail_client.php
+++ gestion/details/detail_client.php
@@ -8,11 +8,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Dossiers- CheckMyStars</title>
 
-        <link rel="stylesheet" href="bootstrap 5.3/css/bootstrap.min.css">
-        <link rel="stylesheet" href="./fontawesome-7.1.0/css/all.css">
-        <script src="bootstrap 5.3/js/bootstrap.js"></script>
-        <script src="js/search_inspecteurs.js"></script>
-        <link rel="icon" type="image/x-icon" href="pictures/logosm.png">
+        <link rel="stylesheet" href="../../bootstrap 5.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../../fontawesome-7.1.0/css/all.css">
+        <script src="../../bootstrap 5.3/js/bootstrap.js"></script>
+        <script src="../../js/search_inspecteurs.js"></script>
+        <link rel="icon" type="image/x-icon" href="../../assets/pictures/logosm.png">
     </head>
 
     <body class="bg-secondary">
