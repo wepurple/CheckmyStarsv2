b40f7b2fbf2c70164faeedef7fd74fd316d126dc
terence-git <temartinant@stpbb.org>
2026-01-30 14:41:15 +0100

    fix

--- gestion/gestion_dossiers.php
+++ gestion/gestion_dossiers.php
@@ -195,7 +195,7 @@
 
                                 <div class="form-floating mb-3">
                                     <?php
-                                        require_once('./includes/mariadb.php');
+                                        require_once('../includes/mariadb.php');
                                         
                                         $database = new Database();
                                         $db = $database->getConnection();
