a9665d30cd30e9d29a21ed24eb9faed1caea767c
terence-git <temartinant@stpbb.org>
2026-01-30 14:46:02 +0100

    fix

--- criteres/critereBack.php
+++ criteres/critereBack.php
@@ -87,11 +87,11 @@ $starColors = [
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Gestion inspecteurs - CheckMyStars</title>
 
-        <link rel="stylesheet" href="bootstrap 5.3/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-        <link rel="stylesheet" href="bootstrap%205.3/css/style1.css">
-        <script src="bootstrap 5.3/js/bootstrap.js"></script>
-        <link rel="icon" type="image/x-icon" href="pictures/logosm.png">
+        <link rel="stylesheet" href="../bootstrap 5.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+        <link rel="stylesheet" href="../bootstrap%205.3/css/style1.css">
+        <script src="../bootstrap 5.3/js/bootstrap.js"></script>
+        <link rel="icon" type="image/x-icon" href="../assets/pictures/logosm.png">
     </head>
 
     <body class="bg-secondary">
