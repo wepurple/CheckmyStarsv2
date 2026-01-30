64e91518a685138d5363066299e210a3a6a7f5e6
Server <serv@mail.com>
2026-01-30 14:03:42 +0100

    gros bordel

--- testmail.php
+++
@@ -1,9 +0,0 @@
-<?php
-    for ($i=0;$i<9999999999;$i++){
-        mail(
-                "nowattin@stpbb.org",
-                $i."JJBA >>> JJK",
-                "t moche ez get good we are wharlie kirk what a save",
-        );
-    }
-?>
