bf0c38bc23a50e9ce41cf2e2fba8903d8faec3e5
DVZAngelNCTS <angelnctseuw@gmail.com>
2026-02-04 13:07:49 +0100

    fix

--- dashboard.php
+++ dashboard.php
@@ -184,22 +184,22 @@
                         <form id="addSocieteForm">
                         <div class="row g-3">
                             <div class="col-md-6 form-floating">
-                            <input type="text" class="form-control" id="societeNom" placeholder="Nom société" required>
-                            <label for="societeNom">Nom société *</label>
+                                <input type="text" class="form-control" id="societeNom" placeholder="Nom société" required>
+                                <label for="societeNom">Nom société *</label>
                             </div>
                             <div class="col-md-6 form-floating">
-                            <input type="email" class="form-control" id="societeMail" placeholder="mail@societe.fr">
-                            <label for="societeMail">Email société</label>
+                                <input type="email" class="form-control" id="societeMail" placeholder="mail@societe.fr">
+                                <label for="societeMail">Email société</label>
                             </div>
                             <div class="col-12 form-floating">
-                            <input type="text" class="form-control" id="societeTel" placeholder="01 23 45 67 89">
-                            <label for="societeTel">Téléphone</label>
+                                <input type="text" class="form-control" id="societeTel" placeholder="01 23 45 67 89">
+                                <label for="societeTel">Téléphone</label>
                             </div>
 
                             <div class="col-12">
-                            <label class="form-label">Adresse complète</label>
-                            <input type="text" class="form-control form-control-lg" id="societeAdresseComplete" placeholder="Tapez votre adresse...">
-                            <small class="text-muted">API officielle France</small>
+                                <label class="form-label">Adresse complète *</label>
+                                <input type="text" class="form-control form-control-lg" id="societeAdresseComplete" placeholder="Tapez votre adresse...">
+                                <small class="text-muted">Ex: 8 Boulevard du Port, 95000 Cergy</small>
                             </div>
 
                             <input type="hidden" id="societeNumRue">
@@ -209,8 +209,8 @@
                             <input type="hidden" id="societePays">
 
                             <div class="col-md-6 form-floating">
-                            <input type="text" class="form-control" id="societeComplement" placeholder="Bât. A, 2ème étage">
-                            <label for="societeComplement">Complément d'adresse</label>
+                                <input type="text" class="form-control" id="societeComplement" placeholder="Bât. A, 2ème étage">
+                                <label for="societeComplement">Complément d'adresse</label>
                             </div>
                         </div>
                         </form>
