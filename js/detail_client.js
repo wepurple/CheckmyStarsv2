111af04ccbd6bb1e4ba0edaf86169b0342ce1256
DVZAngelNCTS <angelnctseuw@gmail.com>
2026-02-04 13:47:45 +0100

    test id

--- js/detail_client.js
+++ js/detail_client.js
@@ -1,1 +1,13 @@
-console.log("ok")
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    var clientLastName = document.getElementById("leNom");
+    var clientFirstName = document.getElementById("leNom");
+    var clientCompany = document.getElementById("laSociete");
+
+    const currentUrl = window.location.search;
+    var query = new URLSearchParams(currentUrl);
+    var clientId = query.get('id');
+
+    console.log(clientId);
+});
