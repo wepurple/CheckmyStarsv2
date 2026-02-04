ea443e8b834afeaec2ff6bd726a120fc38594c6f
DVZAngelNCTS <angelnctseuw@gmail.com>
2026-02-04 13:51:30 +0100

    test clientinfo

--- js/detail_client.js
+++ js/detail_client.js
@@ -1,6 +1,5 @@
-
-
-document.addEventListener("DOMContentLoaded", () => {
+function preFillClientInfo()
+{
     var clientLastName = document.getElementById("leNom");
     var clientFirstName = document.getElementById("leNom");
     var clientCompany = document.getElementById("laSociete");
@@ -9,5 +8,23 @@ document.addEventListener("DOMContentLoaded", () => {
     var query = new URLSearchParams(currentUrl);
     var clientId = query.get('id');
 
-    console.log(clientId);
+    var clientInformation = getUserById(clientId);
+    console.log(clientInformation);
+}
+
+async function getUserById(id) {
+    const url = "../models/Read/users.php?IdPersonne=" + id;
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            'Content-Type': "application/json"
+        }
+    });
+    const result = await response.json();
+
+    return result;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    preFillClientInfo()
 });
