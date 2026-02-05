5bdd660ce3d0181c41acd03b5819d9b145df9ece
Server <serv@mail.com>
2026-02-05 14:22:23 +0100

    dark mode enhanced

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,14 +1,8 @@
-<style>
-    /* couleur de la navbar*/
-    .navbar{
-        background: #00165e;
-    }
-</style>
-<nav class="sticky-top navbar navbar-expand-lg nav-underline">
+<nav class="sticky-top navbar navbar-expand-lg nav-underline border-bottom border-3 bg-dark" data-bs-theme="dark">
 
     <div class="container-fluid">
 
-        <a class="navbar-brand text-light" href="/checkmystars/admin">
+        <a class="navbar-brand " href="/checkmystars/admin">
             <img src="/CheckMyStars/assets/pictures/logosm.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
             CheckMyStars
         </a>
@@ -27,7 +21,7 @@
                 </li>
             -->
                 <li class="nav-item">
-                    <a class="text-light nav-link<?php if(strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/dashboard.php"){ echo ' active'; } ?>" href="/checkmystars/dashboard">
+                    <a class=" nav-link<?php if(strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/dashboard.php"){ echo ' active'; } ?>" href="/checkmystars/dashboard">
                         Tableau de bord
                     </a>
                 </li>
@@ -35,14 +29,14 @@
             <?php if($_SESSION['Role']['Administrateur']){ ?>
             
                 <li class="nav-item">
-                    <a class="text-light nav-link<?php if(strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/gestion/gestion_utilisateurs.php"){ echo ' active'; } ?>" href="/checkmystars/gestion/gestion_utilisateurs">
+                    <a class=" nav-link<?php if(strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/gestion/gestion_utilisateurs.php"){ echo ' active'; } ?>" href="/checkmystars/gestion/gestion_utilisateurs">
                         Gestion des utilisateurs
                     </a>
                 </li>
 
              
                 <li class="nav-item">
-                    <a class="text-light nav-link<?php if(strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/criteres/critereback.php" || strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/criteres/criterebackstar.php"){ echo ' active'; } ?>" href="/checkmystars/criteres/critereBack">
+                    <a class=" nav-link<?php if(strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/criteres/critereback.php" || strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/criteres/criterebackstar.php"){ echo ' active'; } ?>" href="/checkmystars/criteres/critereBack">
                         Gestion des critères
                     </a>
                 </li>
@@ -52,13 +46,13 @@
             <?php if($_SESSION['Role']['Inspecteur'] || $_SESSION['Role']['Administrateur']){ ?>
 
                 <li class="nav-item">
-                    <a class="text-light nav-link<?php if(strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/gestion/gestion_dossiers.php" || strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/front_dossier.php"){ echo ' active'; } ?>" href="/checkmystars/gestion/gestion_dossiers">
+                    <a class=" nav-link<?php if(strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/gestion/gestion_dossiers.php" || strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/front_dossier.php"){ echo ' active'; } ?>" href="/checkmystars/gestion/gestion_dossiers">
                         Gestion des dossiers
                     </a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="text-light nav-link<?php if(strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/factures/facture.php"){ echo ' active'; } ?>" href="/checkmystars/factures/facture">
+                    <a class=" nav-link<?php if(strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/factures/facture.php"){ echo ' active'; } ?>" href="/checkmystars/factures/facture">
                         Factures et devis
                     </a>
                 </li>
@@ -71,14 +65,14 @@
 
             <ul class="navbar-nav ms-2 mb-2 mb-lg-0">
 
-                <li class="nav-item text-light">
-                    <a class="text-light nav-link<?php if(strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/profil.php"){ echo ' active'; } ?>"<?php if(strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/profil.php"){ echo ' aria-current="page"'; } ?> href = "/checkmystars/profil">
+                <li class="nav-item ">
+                    <a class=" nav-link<?php if(strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/profil.php"){ echo ' active'; } ?>"<?php if(strtolower($_SERVER["PHP_SELF"]) == "/checkmystars/profil.php"){ echo ' aria-current="page"'; } ?> href = "/checkmystars/profil">
                         Connecté<?php if(isset($_SESSION['Prenom']) && isset($_SESSION['Nom'])){ echo " en tant que " . $_SESSION['Prenom'] . " " . $_SESSION['Nom']; } ?>
                     </a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="text-light nav-link" href="/checkmystars/deco">Se déconnecter</a>
+                    <a class=" nav-link" href="/checkmystars/deco">Se déconnecter</a>
                 </li>
 
             </ul>
