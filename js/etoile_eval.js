9e1d2ce0dbceffe82af860d90fd33ff72b6df578
Kylian <kylebigot@stpbb.org>
2026-02-05 08:56:34 +0100

    value

--- js/etoile_eval.js
+++ js/etoile_eval.js
@@ -183,11 +183,11 @@ function Evaluer() {
         const checkbox = document.getElementById(`checkbox-${j}`);
         const status = document.getElementById(`statut-${j}`);
         const points = Number(document.getElementById(`points-${j}`).textContent);
-        const textarea = document.getElementById(`textarea-${j}`.textContent);
+        const textarea = document.getElementById(`textarea-${j}`.value);
         console.log('Checkbox ', j, ': ', checkbox.checked);
         console.log('statut ', j, ': ', status.textContent);
         console.log('points ', j, ': ', points);
-        console.log('textarea ', j, ': ', textarea);
+        console.log('textarea ', j, ': ', textarea.value);
         if (status.textContent === "O") {
             points_O_Max += points;
         }
