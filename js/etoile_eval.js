bc779094208914673173da5a9bbe32be2d75f30a
Kylian <kylebigot@stpbb.org>
2026-02-05 14:25:29 +0100

    test

--- js/etoile_eval.js
+++ js/etoile_eval.js
@@ -245,6 +245,25 @@ function Evaluer() {
         result = "Évaluation valide.";
     }
 
+    const selection = recupererSelection();
+
+    fetch('../models/Update/updateEvaluation.php', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(selection)
+    })
+    .then(response => response.json())
+    .then(result => {
+        console.log('Réponse du serveur : ', result);
+        alert(result.message);
+    })
+    .catch(error => {
+        console.error('Erreur lors de la requête : ', error);
+    });
+
+
     console.log('Points O :', points_O);
     console.log('Points X :', points_X);
     console.log('Points NA :', points_NA);
