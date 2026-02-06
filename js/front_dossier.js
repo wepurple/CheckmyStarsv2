dc2da22f41eac41b13a9770db5c86a7644f292da
Midoux <noe@mail.org>
2026-02-06 16:47:32 +0100

    encore une fois

--- js/front_dossier.js
+++ js/front_dossier.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     finalHtml += `
                         <div class="col-12 text-center">
                             <div class="card bg-dark border-secondary shadow-sm mx-auto custom-carousel-card">
-                                <img src="./assets/img/${item.Photo_Lien}" class="card-img-top img-large" 
+                                <img src="${item.Photo_Lien}" class="card-img-top img-large" 
                                      style="cursor:zoom-in; object-fit: cover; height: 300px;"
                                      onclick="handleOpenLightbox('${item.Photo_Lien}', '${item.Photo_ID}')"
                                      alt="Photo">
